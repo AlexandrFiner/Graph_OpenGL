a596bc705862cd5baf8cf8f4b6f8222a3ecb5490
Alexandr Morohin <finersanya@gmail.com>
2021-05-16 14:37:17 +0500

    init
    load files

---
+++ main.cpp
@@ -0,0 +1,24 @@
+﻿#include "Graph_.h"
+
+int main(int argc, char* argv[]) {
+    setlocale(LC_ALL, "Russian");
+
+    makeGraph();
+
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+    glutInitWindowSize(WinW, WinH);
+    glutCreateWindow("Графы");
+    glClearColor(1.0, 1.0, 1.0, 1.0);
+    WinW = glutGet(GLUT_WINDOW_WIDTH);
+    WinH = glutGet(GLUT_WINDOW_HEIGHT);
+
+
+    glutDisplayFunc(display);
+    glutReshapeFunc(reshape);
+    glutMotionFunc(mouseMove);
+    glutPassiveMotionFunc(mouseMove);
+    glutMouseFunc(mouseClick);
+    glutMainLoop();
+    return 0;
+}
